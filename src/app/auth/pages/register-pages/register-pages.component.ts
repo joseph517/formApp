8c9ba041b9303d8794d0bb57e9e0931d73679d0a
Alvaro vergara <avergara@lsv-tech.com>
2024-01-16 18:01:23 -0500

    service to valid field in auth  added

--- src/app/auth/pages/register-pages/register-pages.component.ts
+++ src/app/auth/pages/register-pages/register-pages.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/components/validators/validators';
+import { ValidatorsService } from 'src/app/shared/service/validator.service';
 
 @Component({
   templateUrl: './register-pages.component.html',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPagesComponent {
+
+  constructor(
+    private fb: FormBuilder,
+    private ValidatorsService: ValidatorsService
+    ){}
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.ValidatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.ValidatorsService.emailPattern)]],
+    username: ['', [Validators.required, this.ValidatorsService.canBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  isValidfield(field: string){
+    return this.ValidatorsService.isValidField(this.myForm, field )
+  }
+
+  onSubmit(){
+    console.log('hola');
+
+    this.myForm.markAllAsTouched()
+  }
 
 }
