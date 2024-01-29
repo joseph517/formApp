df83b70cecf2bac62abc03e0f58e14239a946cc7
Alvaro vergara <avergara@lsv-tech.com>
2024-01-29 17:59:36 -0500

    validators

--- src/app/auth/pages/register-pages/register-pages.component.ts
+++ src/app/auth/pages/register-pages/register-pages.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 // import { emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/components/validators/validators';
 import { ValidatorsService } from 'src/app/shared/service/validator.service';
+import { EmailValidatorService } from 'src/app/shared/service/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-pages.component.html',
@@ -17,7 +19,7 @@ export class RegisterPagesComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.ValidatorsService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.ValidatorsService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.ValidatorsService.emailPattern)], [new EmailValidatorService()]],
     username: ['', [Validators.required, this.ValidatorsService.canBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required, Validators.minLength(6)]],
