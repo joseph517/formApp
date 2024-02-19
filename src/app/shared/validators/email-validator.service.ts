a1d8c9cf9009596e84d12c59795d16fb037d840f
Alvaro vergara <avergara@lsv-tech.com>
2024-02-19 17:52:20 -0500

    validator email

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { Observable, delay, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailValidatorService implements AsyncValidator{
@@ -8,11 +8,22 @@ export class EmailValidatorService implements AsyncValidator{
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
     const email = control.value
-    console.log(email);
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors | null>((subcriber)=>{
+      console.log({email});
+      if(email == 'alvaro@google.com'){
+        subcriber.next({emailTaken: true})
+        subcriber.complete()
+        return
+      }
+      subcriber.next(null)
+      subcriber.complete()
+
     })
+    return httpCallObservable
+
   }
+
+
 
   // validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
